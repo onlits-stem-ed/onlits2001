f75aa2ae21078720214785c2d7b56319b4e7a69b
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-01-11 16:59:13 +0530

    20230111

--- C/Structure/Hotel.c
+++ C/Structure/Hotel.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define MAX 5
+#define MAX 10
 
 typedef struct
 {
@@ -12,29 +12,75 @@ typedef struct
 
 int init(HOTEL master[], int *size);
 void display(const HOTEL hotel[], const int size);
+void filter_grade(const HOTEL *master, const int in_size, HOTEL *result, int *out_size);
+void sort_room_charge(HOTEL *hotel, const int size);
 
 int main(void)
 {
-    HOTEL master[MAX];
-    int size = 0;
+    HOTEL master[MAX], result[MAX];
+    int size = 0, out_size = 0;
+    //------------------------ Initialization ------------------------------------
     init(master, &size);
+    printf("--------------------------- MASTER DATA ------------------------------\n");
+    sort_room_charge(master, size);
     display(master, size);
+
+    //--------------------------- Testing Objective 1 -----------------------------
+    printf("--------------------------- OBJECTIVE 1 ------------------------------\n");
+    filter_grade(master, size, result, &out_size);
+    sort_room_charge(result, out_size);
+    display(result, out_size);
     return 0;
 }
 
-int init(HOTEL master[], int *size) //To make testing easy
+int init(HOTEL master[], int *size) // To make testing easy
 {
     FILE *fp;
     fp = fopen("Hotel.dat", "r");
-    fscanf(fp, "%s,%s,%c,%f,%d\n", master[0].name, master[0].address, master[0].grade, master[0].avg_room_charge, master[0].no_of_rooms);
-    (*size)++;
+    while (!feof(fp))
+    {
+        fscanf(fp, "%[^,],%[^,],%c,%f,%d\n", master[*size].name, master[*size].address, &master[*size].grade, &master[*size].avg_room_charge, &master[*size].no_of_rooms);
+        (*size)++;
+    }
 }
 
 void display(const HOTEL hotel[], const int size)
 {
-    printf("%s%s%s%s%s\n", "Name", "Address", "Grade", "Average Room Charge", "Number of Rooms");
-    for(int i=0; i<size; i++)
+    printf("%-50s%-50s%-20s%20s%20s\n", "Name", "Address", "Grade", "Average Room Charge", "Number of Rooms");
+    for (int i = 0; i < size; i++)
     {
-        printf("%s%s%c%f%d\n",hotel[i].name, hotel[i].address, hotel[i].grade, hotel[i].avg_room_charge,hotel[i].no_of_rooms);
+        printf("%-50s%-50s%-10c%20.2f%20d\n", hotel[i].name, hotel[i].address, hotel[i].grade, hotel[i].avg_room_charge, hotel[i].no_of_rooms);
+    }
+}
+
+void filter_grade(const HOTEL *master, const int in_size, HOTEL *result, int *out_size)
+{
+    char grade;
+    *out_size = 0;
+    printf("Enter grade: ");
+    scanf("%c", &grade);
+    for (int i = 0; i < MAX; i++)
+    {
+        if (master[i].grade == grade)
+        {
+            result[*out_size] = master[i];
+            (*out_size)++;
+        }
+    }
+}
+
+void sort_room_charge(HOTEL *hotel, const int size)
+{
+    for(int i=0; i<size-1; i++)
+    {
+        for(int j=i+1; j<size; j++)
+        {
+            if(hotel[i].avg_room_charge > hotel[j].avg_room_charge)
+            {
+                HOTEL t = hotel[i];
+                hotel[i] = hotel[j];
+                hotel[j] = t;
+            }
+        }
     }
 }
